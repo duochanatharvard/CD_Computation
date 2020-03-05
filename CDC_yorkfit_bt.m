79b9b2619bc67af289861412fd334c8a15252dd4
Duo Chan <duochan@dhcp-10-250-200-244.harvard.edu>
2020-03-05 11:08:08 -0500

    Add block bootstrapping to CDC_yorkfit_bt

--- CDC_yorkfit_bt.m
+++ CDC_yorkfit_bt.m
@@ -1,11 +1,15 @@
 % [slope, inter, slope_member, inter_member] =  ...
-%                   CDC_yorkfit_bt(field_y,field_x,sigma_y,sigma_x,r,dim,N)
+%                   CDC_yorkfit_bt(field_y,field_x,sigma_y,sigma_x,r,dim,N,P)
 % 
 % output variables are:
 % - output:     variance
 % - l_effect:   effective sample size
 % - out_member: bootstrap member of variance
 % - out_std:    std of bootstrapped variance
+%
+% Other input options:
+% P.mute_output
+% P.N_block
 % 
 % CDC_yorkfit_bt computes the linear regression: y = ax + b
 % with uncertainty in both x and y in certain dimension of matrices
@@ -15,7 +19,7 @@
 % 
 % Again, in these fitting related functions, dimensionality is not omittable!
 % 
-% Last update: 2018-08-23
+% Last update: 2020-03-05
 
 function [slope, inter, slope_member, inter_member] =  ...
             CDC_yorkfit_bt(field_y,field_x,sigma_y,sigma_x,r,dim,N,varargin)
@@ -30,26 +34,32 @@ function [slope, inter, slope_member, inter_member] =  ...
     dim_1 = size(field_y)*0 + 1;   dim_1(dim) = size(field_y,dim); 
     dim_2 = size(field_y);         dim_2(dim) = 1;
     
-    if numel(size(field_x)) == 2,
+    if numel(size(field_x)) == 2
         field_x = repmat(reshape(field_x,dim_1),dim_2);
     end
     
-    if numel(sigma_x) == 1,
+    if numel(sigma_x) == 1
         sigma_x = repmat(sigma_x,size(field_x));
-    elseif numel(size(sigma_x)) == 2,
+    elseif numel(size(sigma_x)) == 2
         sigma_x = repmat(reshape(sigma_x,dim_1),dim_2);
     end
 
-    if numel(sigma_y) == 1,
+    if numel(sigma_y) == 1
         sigma_y = repmat(sigma_y,size(field_y));
-    elseif numel(size(sigma_y)) == 2,
+    elseif numel(size(sigma_y)) == 2
         sigma_y = repmat(reshape(sigma_y,dim_1),dim_2);
     end
     
-    if numel(r) == 1,
+    if numel(r) == 1
         r = repmat(r,size(field_x));
-    elseif numel(size(r)) == 2,
+    elseif numel(size(r)) == 2
         r = repmat(reshape(r,dim_1),dim_2);
+    end
+
+    if isfield(P,'N_block')
+        N_block =  P.N_block;
+    else
+        N_block = 1;
     end
 
     % **************************************************
@@ -61,7 +71,7 @@ function [slope, inter, slope_member, inter_member] =  ...
     % Bootstrap for the order 
     % **************************************************
     rng(0);
-    [~,boot_sample] = bootstrp(N, @(x) [mean(x)], [1:size(field_y,dim)]);
+    [~,boot_sample] = bootstrp(N, @(x) [mean(x)], [1:fix(size(field_y,dim)/N_block)+1]);
     
     % **************************************************
     % Re-sample and estimate variances
@@ -84,11 +94,16 @@ function [slope, inter, slope_member, inter_member] =  ...
             if rem(ct,round(N/10)) == 0,  disp(num2str(ct)); end
         end
         
-        field_yy = CDC_subset(field_y,dim,boot_sample(:,ct));
-        field_xx = CDC_subset(field_x,dim,boot_sample(:,ct));
-        sigma_yy = CDC_subset(sigma_y,dim,boot_sample(:,ct));
-        sigma_xx = CDC_subset(sigma_x,dim,boot_sample(:,ct));
-        rr       = CDC_subset(r      ,dim,boot_sample(:,ct));
+        order = repmat(boot_sample(:,ct)'*N_block-N_block,N_block,1);
+        order = order + repmat([1:N_block]',1,size(boot_sample,1));
+        order = order(:);
+        order(order > size(field_y,dim)) = [];
+
+        field_yy = CDC_subset(field_y,dim,order);
+        field_xx = CDC_subset(field_x,dim,order);
+        sigma_yy = CDC_subset(sigma_y,dim,order);
+        sigma_xx = CDC_subset(sigma_x,dim,order);
+        rr       = CDC_subset(r      ,dim,order);
 
         [slope_temp, inter_temp] = ...
             CDC_yorkfit(field_yy,field_xx,sigma_yy,sigma_xx,rr,dim);
@@ -102,9 +117,9 @@ function [slope, inter, slope_member, inter_member] =  ...
     rep = size(slope_member);
     rep(1:dim_2-1) = 1;
     med = quantile(slope_member,0.5,dim_2);
-    slope_member = slope_member + repmat(slope - med,rep);
+    % slope_member = slope_member + repmat(slope - med,rep);
     med = quantile(inter_member,0.5,dim_2);
-    inter_member = inter_member + repmat(inter - med,rep);
+    % inter_member = inter_member + repmat(inter - med,rep);
     
     slope_member = squeeze(slope_member);
     inter_member = squeeze(inter_member);
