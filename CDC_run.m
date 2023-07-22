1da79d6b76523c3b74836cd2224b0b920c877504
duochanatharvard <duochan@.harvard.edu>
2023-07-22 11:49:17 +0800

    Update CDC_run.m

--- CDC_run.m
+++ CDC_run.m
@@ -2,28 +2,35 @@
 
 function output = CDC_run(input,N,dim)
 
+    if ~exist('N','var'), N = 3; end
+    if isempty(N),        N = 3; end
+
+    if any(size(input) == 1)
+        input = reshape(input,numel(input),1);
+        dim   = 1;
+    end
+
+    if ~exist('dim','var'), error('dim not assigned!'); end
+    if isempty(dim),        error('dim not assigned!'); end
+
     output = nan(size(input));
 
     NN = size(input,dim);
-    
+    if rem(N,2) == 0
+        error('N has to be an odd number');
+    else
+        Nh = (N + 1)/2;
+    end
+
     for ct = 1:NN
 
-        if ismember(ct,((N+1)/2): (NN - (N-1)/2)),
-            list = (ct-(N-1)/2):(ct+(N-1)/2);
-            
-        elseif ismember(ct,1:((N-1)/2)),
-            list = 1:(ct+(N-1)/2);
-            
-        elseif ismember(ct,(NN - (N-1)/2):NN),
-            list =  (ct - (N-1)/2):NN;
-        end
+        list            = (-Nh:Nh) + ct;
+        list(list < 1)  = [];
+        list(list > NN) = [];
         
-        input_sub = CDC_subset(input,dim,list);
-        temp = nanmean(input_sub,dim);
-        output = CDC_assign(output,temp,dim,ct); 
+        input_sub    = CDC_subset(input,dim,list);
+        temp         = nanmean(input_sub,dim);
+        output       = CDC_assign(output,temp,dim,ct); 
         
     end  
 end
-
-    
-    
